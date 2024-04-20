998d860551be9d2e58402421c1ddd02dca587a95
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-04-20 20:15:57 +0400

    Add files via upload

---
+++ funkzia_parametrom.py
@@ -0,0 +1,9 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(a = 2, b = 3)
+print_params(c = 3)
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
